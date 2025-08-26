57fd8b1a5e12ccc7c4fd629427529c8f80639dd7
Akshay Kumar <rmakshaykumar08@gmail.com>
2025-08-26 08:44:53 -0500

    feat: implement routing and added portfolio details

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Blog from './pages/Blogs/pages';
+import Home from './pages/Home/pages';
+
 
 function App() {
   return (
-    <Blog />
+    <Router basename="/g">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
